78d7f9d988a0a524cbdfa820af25e9c58a902f6b
Uy101112 <uy265348@gmail.com>
2023-02-12 19:46:28 +0530

    hakjdakj

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2023-02-15T10:35:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2023-02-14T10:35:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-02-12T10:35:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1100; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xAb18a4368Df6b09236839CE10aF1f86181dd58c7"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
